80e3ea5f42c2d47a6c08fe60821433e9f091423d
Jack <chen86860@126.com>
2016-08-08 20:56:14 +0800

    fix mongondb

--- models/mongonDb.js
+++ models/mongonDb.js
@@ -11,3 +11,40 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
     //succeed
+});
+
+var articleSchema = new Schema({
+    id: String,
+    username: String,
+    createTime: String,
+    title: String,
+    contentSummary: String,
+    content: String
+});
+
+//Blog schema
+var blogSchema = new Schema({
+    uid: String,
+    username: String,
+    password: String,
+    email: String,
+    createTime: String,
+    createRegionIP: String
+});
+
+
+//userprofile
+var userinfo = new Schema({
+    uid: String,
+    avar: String,
+    telNum: String,
+    IMIE: String
+});
+
+
+//向外界提供接口
+exports.userDoc = mongoose.model('userDoc', blogSchema);
+
+exports.userprofile = mongoose.model('userprofile', userinfo);
+
+exports.article = mongoose.model('articles', articleSchema);
