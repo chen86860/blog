a497629226a50c2bb83719df69c8c0b73b69bc80
Jack <chen86860@126.com>
2019-04-14 18:21:25 +0800

    ✨ remove useless pages

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  "extends": ["standard", "react-app"],
-  "rules": {
-    "comma-dangle": 0,
-    "space-before-function-paren": 0,
-    "jsx-a11y/href-no-hash": "off",
-    "jsx-a11y/anchor-is-valid": ["warn", { "aspects": ["invalidHref"] }]
-  }
-}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Blog
-inti Blog
+# Jack's Blog
+
+> My Own Swordsman
+
+## **2018**
+
+### August
+
+- August 3, 2018 ~ [盘点 React 16.0 ~ 16.5 主要更新及其应用](https://github.com/chen86860/blog/issues/2)
